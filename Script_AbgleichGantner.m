4724bc75e686b95d432337ddb657d4ed351adc6f
gui1303iwes <guilherme.reis@iwes.fraunhofer.de>
2023-09-22 11:13:13 +0200

    Update index finding
    Changed the interp1 for the find function
    Added comments and questions

--- Script_AbgleichGantner.m
+++ Script_AbgleichGantner.m
@@ -5,6 +5,7 @@ time_manner = Channel10_time;
 timeDMS = DMS1.XData;
 begintime_manner = datetime(2023,08,23,12,45,27);
 time_abs_manner = begintime_manner + seconds(time_manner);
+tol     = 0.000001; % tolerance to use with the find function
 %begintime_DMS = datetime(2023,08,23,11,03,18,779000);
 %Timesync noch etwas angepasst
 begintime_DMS = datetime(2023,08,23,10,50,18,779000);
@@ -33,111 +34,130 @@ subplot(2,1,2);
 plot(time_abs_DMS, DMS2.YData);
 ylim([-0.25, 0.25]);
 legend('Gantner')
-%%
-%Telemetrie
+%% Telemetrie (manner)
+% Find the time intervals where the bolt is connected to the telemetrie
+% system. This is done by:
+% 1- Define the left and right limits (start and end of the connection).
+% Bases on the Excel sheet ??
+% 2- Use the find function to find the indexes of these limits on the time
+% vector
+% 3- Isolate the bolt signals only within this interval
+% 4- Do this for each one of the 4 bolts and with 0 and 390Nm pre-tension
 
 ll_Bolt1_0N = datetime(2023,08,23,12,46,00);
 rr_Bolt1_0N = datetime(2023,08,23,12,48,00);
-inx_ll_Bolt1_0N = interp1(time_abs_manner, 1:length(time_abs_manner), ll_Bolt1_0N, 'nearest');
-inx_rr_Bolt1_0N = interp1(time_abs_manner, 1:length(time_abs_manner), rr_Bolt1_0N, 'nearest');
+inx_ll_Bolt1_0N = find(abs(time_abs_manner - ll_Bolt1_0N)<tol,1,'first');
+inx_rr_Bolt1_0N = find(abs(time_abs_manner - rr_Bolt1_0N)<tol,1,'last');
 Bolt1_0N_ave = mean(Bolt1(inx_ll_Bolt1_0N:inx_rr_Bolt1_0N));
 %
 ll_Bolt1_390N = datetime(2023,08,23,13,24,00);
 rr_Bolt1_390N = datetime(2023,08,23,13,26,00);
-inx_ll_Bolt1_390N = interp1(time_abs_manner, 1:length(time_abs_manner), ll_Bolt1_390N, 'nearest');
-inx_rr_Bolt1_390N = interp1(time_abs_manner, 1:length(time_abs_manner), rr_Bolt1_390N, 'nearest');
+inx_ll_Bolt1_390N = find(abs(time_abs_manner - ll_Bolt1_390N)<tol,1,'first');
+inx_rr_Bolt1_390N = find(abs(time_abs_manner - rr_Bolt1_390N)<tol,1,'last');
 Bolt1_390N_ave = mean(Bolt1(inx_ll_Bolt1_390N:inx_rr_Bolt1_390N));
 %
 ll_Bolt2_0N = datetime(2023,08,23,13,46,30);
 rr_Bolt2_0N = datetime(2023,08,23,13,48,00);
-inx_ll_Bolt2_0N = interp1(time_abs_manner, 1:length(time_abs_manner), ll_Bolt2_0N, 'nearest');
-inx_rr_Bolt2_0N = interp1(time_abs_manner, 1:length(time_abs_manner), rr_Bolt2_0N, 'nearest');
+inx_ll_Bolt2_0N = find(abs(time_abs_manner - ll_Bolt2_0N)<tol,1,'first');
+inx_rr_Bolt2_0N = find(abs(time_abs_manner - rr_Bolt2_0N)<tol,1,'last');
 Bolt2_0N_ave = mean(Bolt2(inx_ll_Bolt2_0N:inx_rr_Bolt2_0N));
 %
+% From this point all the time intervals are outside the time_abs_manner
+% vector
 ll_Bolt2_390N = datetime(2023,08,23,13,54,00);
 rr_Bolt2_390N = datetime(2023,08,23,13,56,00);
-inx_ll_Bolt2_390N = interp1(time_abs_manner, 1:length(time_abs_manner), ll_Bolt2_390N, 'nearest');
-inx_rr_Bolt2_390N = interp1(time_abs_manner, 1:length(time_abs_manner), rr_Bolt2_390N, 'nearest');
+inx_ll_Bolt2_390N = find(abs(time_abs_manner - ll_Bolt2_390N)<tol,1,'first');
+inx_rr_Bolt2_390N = find(abs(time_abs_manner - rr_Bolt2_390N)<tol,1,'last');
 Bolt2_390N_ave = mean(Bolt2(inx_ll_Bolt2_390N:inx_rr_Bolt2_390N));
 %
 ll_Bolt3_0N = datetime(2023,08,23,14,00,00);
 rr_Bolt3_0N = datetime(2023,08,23,14,05,00);
-inx_ll_Bolt3_0N = interp1(time_abs_manner, 1:length(time_abs_manner), ll_Bolt3_0N, 'nearest');
-inx_rr_Bolt3_0N = interp1(time_abs_manner, 1:length(time_abs_manner), rr_Bolt3_0N, 'nearest');
+inx_ll_Bolt3_0N = find(abs(time_abs_manner - ll_Bolt3_0N)<tol,1,'first');
+inx_rr_Bolt3_0N = find(abs(time_abs_manner - rr_Bolt3_0N)<tol,1,'last');
 Bolt3_0N_ave = mean(Bolt3(inx_ll_Bolt3_0N:inx_rr_Bolt3_0N));
 %
 ll_Bolt3_390N = datetime(2023,08,23,13,34,00);
 rr_Bolt3_390N = datetime(2023,08,23,13,36,00);
-inx_ll_Bolt3_390N = interp1(time_abs_manner, 1:length(time_abs_manner), ll_Bolt3_390N, 'nearest');
-inx_rr_Bolt3_390N = interp1(time_abs_manner, 1:length(time_abs_manner), rr_Bolt3_390N, 'nearest');
+inx_ll_Bolt3_390N = find(abs(time_abs_manner - ll_Bolt3_390N)<tol,1,'first');
+inx_rr_Bolt3_390N = find(abs(time_abs_manner - rr_Bolt3_390N)<tol,1,'last');
 Bolt3_390N_ave = mean(Bolt3(inx_ll_Bolt3_390N:inx_rr_Bolt3_390N));
 %
 ll_Bolt4_0N = datetime(2023,08,23,14,36,00);
 rr_Bolt4_0N = datetime(2023,08,23,14,38,00);
-inx_ll_Bolt4_0N = interp1(time_abs_manner, 1:length(time_abs_manner), ll_Bolt4_0N, 'nearest');
-inx_rr_Bolt4_0N = interp1(time_abs_manner, 1:length(time_abs_manner), rr_Bolt4_0N, 'nearest');
+inx_ll_Bolt4_0N = find(abs(time_abs_manner - ll_Bolt4_0N)<tol,1,'first');
+inx_rr_Bolt4_0N = find(abs(time_abs_manner - rr_Bolt4_0N)<tol,1,'last');
 Bolt4_0N_ave = mean(Bolt4(inx_ll_Bolt4_0N:inx_rr_Bolt4_0N));
 %
 ll_Bolt4_390N = datetime(2023,08,23,14,00,00);
 rr_Bolt4_390N = datetime(2023,08,23,14,04,00);
-inx_ll_Bolt4_390N = interp1(time_abs_manner, 1:length(time_abs_manner), ll_Bolt4_390N, 'nearest');
-inx_rr_Bolt4_390N = interp1(time_abs_manner, 1:length(time_abs_manner), rr_Bolt4_390N, 'nearest');
+inx_ll_Bolt4_390N = find(abs(time_abs_manner - ll_Bolt4_390N)<tol,1,'first');
+inx_rr_Bolt4_390N = find(abs(time_abs_manner - rr_Bolt4_390N)<tol,1,'last');
 Bolt4_390N_ave = mean(Bolt4(inx_ll_Bolt4_390N:inx_rr_Bolt4_390N));
 
+% Why this is defined differently from the Gantner avg vector ?
 Bolt_av=[Bolt1_0N_ave, Bolt2_0N_ave, Bolt3_0N_ave, Bolt4_0N_ave; Bolt1_390N_ave, Bolt2_390N_ave, Bolt3_390N_ave, Bolt4_390N_ave];
-%%
-%Gantner
+%% Ganter
+% Find the time intervals where the bolt is connected to the Gantner
+% system. This is done by:
+% 1- Define the left and right limits (start and end of the connection).
+% Bases on the Excel sheet ??
+% 2- Use the find function to find the indexes of these limits on the time
+% vector
+% 3- Isolate the bolt signals only within this interval
+% 4- Do this for each one of the 4 bolts and with 0 and 390Nm pre-tension
 
 ll_G_Bolt1_0N = datetime(2023,08,23,12,15,00);
 rr_G_Bolt1_0N = datetime(2023,08,23,12,20,00);
-inx_ll_G_Bolt1_0N = interp1(time_abs_DMS, 1:length(time_abs_DMS), ll_G_Bolt1_0N, 'nearest');
-inx_rr_G_Bolt1_0N = interp1(time_abs_DMS, 1:length(time_abs_DMS), rr_G_Bolt1_0N, 'nearest');
+inx_ll_G_Bolt1_0N = find(abs(time_abs_DMS - ll_G_Bolt1_0N) < tol, 1, 'first');
+inx_rr_G_Bolt1_0N = find(abs(time_abs_DMS - rr_G_Bolt1_0N) < tol, 1, 'last');
 G_Bolt1_0N_ave = mean(DMS2.YData(inx_ll_G_Bolt1_0N:inx_rr_G_Bolt1_0N));
-%
+
 ll_G_Bolt1_390N = datetime(2023,08,23,13,29,00);
 rr_G_Bolt1_390N = datetime(2023,08,23,13,31,00);
-inx_ll_G_Bolt1_390N = interp1(time_abs_DMS, 1:length(time_abs_DMS), ll_G_Bolt1_390N, 'nearest');
-inx_rr_G_Bolt1_390N = interp1(time_abs_DMS, 1:length(time_abs_DMS), rr_G_Bolt1_390N, 'nearest');
+inx_ll_G_Bolt1_390N = find(abs(time_abs_DMS - ll_G_Bolt1_390N) < tol, 1, 'first');
+inx_rr_G_Bolt1_390N = find(abs(time_abs_DMS - rr_G_Bolt1_390N) < tol, 1, 'last');
 G_Bolt1_390N_ave = mean(DMS2.YData(inx_ll_G_Bolt1_390N:inx_rr_G_Bolt1_390N));
-%
+
 ll_G_Bolt2_0N = datetime(2023,08,23,13,42,00);
 rr_G_Bolt2_0N = datetime(2023,08,23,13,44,00);
-inx_ll_G_Bolt2_0N = interp1(time_abs_DMS, 1:length(time_abs_DMS), ll_G_Bolt2_0N, 'nearest');
-inx_rr_G_Bolt2_0N = interp1(time_abs_DMS, 1:length(time_abs_DMS), rr_G_Bolt2_0N, 'nearest');
+inx_ll_G_Bolt2_0N = find(abs(time_abs_DMS - ll_G_Bolt2_0N) < tol, 1, 'first');
+inx_rr_G_Bolt2_0N = find(abs(time_abs_DMS - rr_G_Bolt2_0N) < tol, 1, 'last');
 G_Bolt2_0N_ave = mean(DMS2.YData(inx_ll_G_Bolt2_0N:inx_rr_G_Bolt2_0N));
-%
+
 ll_G_Bolt2_390N = datetime(2023,08,23,13,36,00);
 rr_G_Bolt2_390N = datetime(2023,08,23,13,38,00);
-inx_ll_G_Bolt2_390N = interp1(time_abs_DMS, 1:length(time_abs_DMS), ll_G_Bolt2_390N, 'nearest');
-inx_rr_G_Bolt2_390N = interp1(time_abs_DMS, 1:length(time_abs_DMS), rr_G_Bolt2_390N, 'nearest');
+inx_ll_G_Bolt2_390N = find(abs(time_abs_DMS - ll_G_Bolt2_390N) < tol, 1, 'first');
+inx_rr_G_Bolt2_390N = find(abs(time_abs_DMS - rr_G_Bolt2_390N) < tol, 1, 'last');
 G_Bolt2_390N_ave = mean(DMS2.YData(inx_ll_G_Bolt2_390N:inx_rr_G_Bolt2_390N));
-%
+
 ll_G_Bolt3_0N = datetime(2023,08,23,14,07,40);
 rr_G_Bolt3_0N = datetime(2023,08,23,14,09,20);
-inx_ll_G_Bolt3_0N = interp1(time_abs_DMS, 1:length(time_abs_DMS), ll_G_Bolt3_0N, 'nearest');
-inx_rr_G_Bolt3_0N = interp1(time_abs_DMS, 1:length(time_abs_DMS), rr_G_Bolt3_0N, 'nearest');
+inx_ll_G_Bolt3_0N = find(abs(time_abs_DMS - ll_G_Bolt3_0N) < tol, 1, 'first');
+inx_rr_G_Bolt3_0N = find(abs(time_abs_DMS - rr_G_Bolt3_0N) < tol, 1, 'last');
 G_Bolt3_0N_ave = mean(DMS2.YData(inx_ll_G_Bolt3_0N:inx_rr_G_Bolt3_0N));
-%
+
 ll_G_Bolt3_390N = datetime(2023,08,23,13,53,00);
 rr_G_Bolt3_390N = datetime(2023,08,23,13,55,00);
-inx_ll_G_Bolt3_390N = interp1(time_abs_DMS, 1:length(time_abs_DMS), ll_G_Bolt3_390N, 'nearest');
-inx_rr_G_Bolt3_390N = interp1(time_abs_DMS, 1:length(time_abs_DMS), rr_G_Bolt3_390N, 'nearest');
+inx_ll_G_Bolt3_390N = find(abs(time_abs_DMS - ll_G_Bolt3_390N) < tol, 1, 'first');
+inx_rr_G_Bolt3_390N = find(abs(time_abs_DMS - rr_G_Bolt3_390N) < tol, 1, 'last');
 G_Bolt3_390N_ave = mean(DMS2.YData(inx_ll_G_Bolt3_390N:inx_rr_G_Bolt3_390N));
-%
+
 ll_G_Bolt4_0N = datetime(2023,08,23,14,28,00);
 rr_G_Bolt4_0N = datetime(2023,08,23,14,32,00);
-inx_ll_G_Bolt4_0N = interp1(time_abs_DMS, 1:length(time_abs_DMS), ll_G_Bolt4_0N, 'nearest');
-inx_rr_G_Bolt4_0N = interp1(time_abs_DMS, 1:length(time_abs_DMS), rr_G_Bolt4_0N, 'nearest');
+inx_ll_G_Bolt4_0N = find(abs(time_abs_DMS - ll_G_Bolt4_0N) < tol, 1, 'first');
+inx_rr_G_Bolt4_0N = find(abs(time_abs_DMS - rr_G_Bolt4_0N) < tol, 1, 'last');
 G_Bolt4_0N_ave = mean(DMS2.YData(inx_ll_G_Bolt4_0N:inx_rr_G_Bolt4_0N));
-%
+
 ll_G_Bolt4_390N = datetime(2023,08,23,14,21,05);
 rr_G_Bolt4_390N = datetime(2023,08,23,14,22,50);
-inx_ll_G_Bolt4_390N = interp1(time_abs_DMS, 1:length(time_abs_DMS), ll_G_Bolt4_390N, 'nearest');
-inx_rr_G_Bolt4_390N = interp1(time_abs_DMS, 1:length(time_abs_DMS), rr_G_Bolt4_390N, 'nearest');
+inx_ll_G_Bolt4_390N = find(abs(time_abs_DMS - ll_G_Bolt4_390N) < tol, 1, 'first');
+inx_rr_G_Bolt4_390N = find(abs(time_abs_DMS - rr_G_Bolt4_390N) < tol, 1, 'last');
 G_Bolt4_390N_ave = mean(DMS2.YData(inx_ll_G_Bolt4_390N:inx_rr_G_Bolt4_390N));
 
+% Vector with avg values
 G_Bolt_av=[G_Bolt1_0N_ave, G_Bolt2_0N_ave, G_Bolt3_0N_ave, G_Bolt4_0N_ave; G_Bolt1_390N_ave, G_Bolt2_390N_ave, G_Bolt3_390N_ave, G_Bolt4_390N_ave];
 
+% Why these steps have not been made for the telemetrie?
 GBolt4_diff = G_Bolt4_390N_ave-G_Bolt4_0N_ave;
 GBolt3_diff = G_Bolt3_390N_ave-G_Bolt3_0N_ave;
 GBolt2_diff = G_Bolt2_390N_ave-G_Bolt2_0N_ave;
@@ -146,7 +166,9 @@ GBolt1_diff = G_Bolt1_390N_ave-G_Bolt1_0N_ave;
 GBolt_diff=[GBolt1_diff, GBolt2_diff, GBolt3_diff, GBolt4_diff];
 
 
-%%
+%% Plots
+% what does 'para' mean?
+
 %Bolt1
 y = [G_Bolt1_0N_ave, G_Bolt1_390N_ave];
 x = [Bolt1_0N_ave, Bolt1_390N_ave];
